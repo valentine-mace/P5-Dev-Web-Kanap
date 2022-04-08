75837d79a6fec8546e53ca164e4883d36f052c32
valentine-mace <valentine.mace@hotmail.com>
2022-04-08 19:31:03 +0200

    affichage du produit

--- front/js/product.js
+++ front/js/product.js
@@ -1,7 +1,32 @@
 //on récupère l'id du produit dans l'url
 let current_url = window.location.href;
 var url = new URL(current_url);
-var id = url.searchParams.get("id");
-console.log(id);
+let id = url.searchParams.get("id");
 
+fetch("http://localhost:3000/api/products/" + id)
+	.then(function (res) {
+		if (res.ok) {
+			return res.json();
+		}
+	})
+  .then(function(value){
+		//récupérer les données du produit
+		let product_name = value.name;
+		let product_price = value.price;
+		let product_description = value.description;
+		let product_url = value.imageUrl;
+		let product_alt = value.altTxt;	
+		//let product_colors = value[0].colors;	
 
+    //insérer dans les balises html
+		let nom = document.getElementById('title');
+		nom.innerHTML =  product_name;
+		let prix = document.getElementById('price');
+		prix.innerHTML =  product_price;
+		let description = document.getElementById('description');
+		description.innerHTML =  product_description;
+		let image = document.getElementsByClassName("item__img");
+		image[0].innerHTML = "<img src= \""+product_url+"\" alt= \""+product_alt+"\">";
+		
+
+  })
