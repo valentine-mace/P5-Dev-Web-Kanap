2834bf3b15ea6c796b10387201b09b683588a5a3
valentine-mace <valentine.mace@hotmail.com>
2022-04-08 18:43:28 +0200

    affichage de tous les produits sur la page d'accueil OK

--- front/js/test.js
+++ front/js/test.js
@@ -6,16 +6,45 @@ fetch("http://localhost:3000/api/products")
 		}
 	})
 	.then(function(value){
+		let contenu = "";
 		for (var i = 0; i < value.length; i++){
-			console.log(value[i].price);
+			//récupération de tous les différents attributs de chaque produit depuis la BDD
+			let product_name = value[i].name;
+			let product_description = value[i].description;
+			let product_url = value[i].imageUrl;
+			let product_alt = value[i].altTxt;
+			let product_id = value[i]._id;	
+			contenu = contenu +
+			"<a href=\"./product.html?id=\""+product_id+"\">" +
+			"<article>" +
+			"<img src= \""+product_url+"\" alt= \""+product_description+"\">" +
+			"<h3 class=\"productName\">"+product_name+"</h3>" +
+			"<p class=\"productDescription\">"+product_description+"</p>" +
+			"</article>" +
+			"</a>";
+			//insertion des différents attributs dans la partie html correspondant
+			// let listeNoms = document.getElementsByClassName('productName');
+			// (listeNoms[0]).innerHTML =  product_name;
+
+			// let listeDescription = document.getElementsByClassName('productDescription');
+			// (listeDescription[0]).innerHTML =  product_description;
+
+			// let listeImages = document.querySelector("article");
+			// let image = listeImages.getElementsByTagName("img");
+			// (image[0]).src = product_url;
+			// (image[0]).alt = product_alt;
+			// let item_class = document.getElementsByClassName('items');
+			// let listLinks = item_class[0].querySelector("a")
+			// listLinks.href = "./product.html?id=" + product_id;
+			//console.log(listLinks.href);
+			//console.log(value[i].price);
 		}
+		document.getElementsByClassName("items")[0].innerHTML = contenu;
 	})
 	.catch(function(err){
 		console.log("erreur");
 	});
 
-	let listeEl = document.getElementsByClassName("items");
-	console.log(listeEl);
 
 //récupérer données par catégorie: ici, seulement nom, description, id et image
 
